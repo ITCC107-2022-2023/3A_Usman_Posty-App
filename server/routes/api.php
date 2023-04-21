1b378dd2c7b1958283a25bd4e34fabe56c63884f
Al-Fhaigar J. Usman <93189177+rubickking04@users.noreply.github.com>
2023-04-21 10:44:27 +0800

    coded by Al-Fhaigar Usman

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\HomeController;
+use App\Http\Controllers\API\PostController;
 use App\Http\Controllers\API\Auth\LoginController;
 use App\Http\Controllers\API\Auth\LogoutController;
 use App\Http\Controllers\API\Auth\RegisterController;
@@ -28,12 +29,12 @@ Route::middleware('auth:user-api')->group(function () {
     Route::controller(HomeController::class)->group(function () {
         Route::get('/user', 'auth')->name('login');
     });
-    // Route::controller(PostController::class)->group(function () {
-    //     Route::get('/users/posts', 'index');
-    //     Route::post('/users/posts', 'store');
-    //     Route::put('/users/posts/{id}', 'update');
-    //     Route::delete('/users/posts/{id}', 'destroy');
-    // });
+    Route::controller(PostController::class)->group(function () {
+        Route::get('/users/posts', 'index');
+        Route::post('/users/posts', 'store');
+        Route::put('/users/posts/{id}', 'update');
+        Route::delete('/users/posts/{id}', 'destroy');
+    });
     Route::controller(LogoutController::class)->group(function () {
         Route::post('/logout', 'logout');
     });
