ae849e656257514602a16cf1e4ebfc1c2b52517e
Al-Fhaigar J. Usman <93189177+rubickking04@users.noreply.github.com>
2023-04-21 10:31:21 +0800

    coded by Al-Fhaigar Usman

--- server/app/Http/Controllers/API/Auth/LogoutController.php
+++ server/app/Http/Controllers/API/Auth/LogoutController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class LogoutController extends Controller
 {
-    //
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        Session::flush();
+        Auth::logout();
+        return response()->json(['message' => 'User successfully signed out'], 200);
+    }
 }
--- server/routes/api.php
+++ server/routes/api.php
@@ -2,18 +2,42 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\HomeController;
+use App\Http\Controllers\API\Auth\LoginController;
+use App\Http\Controllers\API\Auth\LogoutController;
+use App\Http\Controllers\API\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Routes for User Authentication
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(RegisterController::class)->group(function () {
+    Route::post('/register', 'register');
+});
+Route::controller(LoginController::class)->group(function () {
+    Route::post('/login', 'login');
+});
+/*
+|--------------------------------------------------------------------------
+| Guards the API Routes for User Authentication
+|--------------------------------------------------------------------------
+*/
+Route::middleware('auth:user-api')->group(function () {
+    Route::controller(HomeController::class)->group(function () {
+        Route::get('/user', 'auth')->name('login');
+    });
+    // Route::controller(PostController::class)->group(function () {
+    //     Route::get('/users/posts', 'index');
+    //     Route::post('/users/posts', 'store');
+    //     Route::put('/users/posts/{id}', 'update');
+    //     Route::delete('/users/posts/{id}', 'destroy');
+    // });
+    Route::controller(LogoutController::class)->group(function () {
+        Route::post('/logout', 'logout');
+    });
+});
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/getusers', 'index');
 });
