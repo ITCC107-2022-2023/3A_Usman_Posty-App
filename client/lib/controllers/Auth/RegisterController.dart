11cde36b4f227d99b9d8dffcc0db29896ab646f4
Al-Fhaigar J. Usman <93189177+rubickking04@users.noreply.github.com>
2023-05-20 20:34:48 +0800

    coded by Al-Fhaigar Usman

--- client/lib/controllers/Auth/RegisterController.dart
+++ client/lib/controllers/Auth/RegisterController.dart
@@ -1,3 +1,11 @@
 import 'package:get/get.dart';
 
-class RegisterController extends GetxController {}
+class RegisterController extends GetxController {
+  final isLoading = false.obs;
+
+  Future register({
+    required String? name,
+    required String? email,
+    required String? password,
+  }) async {}
+}
